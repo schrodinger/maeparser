dc0347751a53081afb39f107762ad0382728475e
Ric <ricrogz@gmail.com>
2018-08-04 14:35:05 -0400

    Fix: set input stream as binary

--- Reader.cpp
+++ Reader.cpp
@@ -1,11 +1,10 @@
 #include "Reader.hpp"
 
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/iostreams/filtering_streambuf.hpp>
 #include <fstream>
 #include <iostream>
-#include <boost/algorithm/string/predicate.hpp>
-#include <boost/iostreams/filtering_streambuf.hpp>
-#include <boost/iostreams/filter/gzip.hpp>
-
 
 using boost::algorithm::ends_with;
 
@@ -16,19 +15,21 @@ namespace mae
 
 Reader::Reader(std::string fname, size_t buffer_size)
 {
-    std::shared_ptr<std::ifstream> stream(new std::ifstream(fname));
-    if(ends_with(fname, ".mae")) {
+    std::shared_ptr<std::ifstream> stream(
+        new std::ifstream(fname, std::ios_base::in | std::ios_base::binary));
+    if (ends_with(fname, ".mae")) {
         m_mae_parser.reset(new MaeParser(stream, buffer_size));
     } else if (ends_with(fname, ".maegz") || ends_with(fname, ".mae.gz")) {
-        m_pregzip_stream = stream;  // Store it since maeparser won't
-        m_gzip_stream.reset(new boost::iostreams::filtering_streambuf<boost::iostreams::input>());
+        m_pregzip_stream = stream; // Store it since maeparser won't
+        m_gzip_stream.reset(new boost::iostreams::filtering_streambuf<
+                            boost::iostreams::input>());
         m_gzip_stream->push(boost::iostreams::gzip_decompressor());
         m_gzip_stream->push(*stream);
-        std::shared_ptr<std::istream> decompressed_stream(new std::istream(m_gzip_stream.get()));
+        std::shared_ptr<std::istream> decompressed_stream(
+            new std::istream(m_gzip_stream.get()));
         m_mae_parser.reset(new MaeParser(decompressed_stream, buffer_size));
     }
 }
-
 
 std::shared_ptr<Block> Reader::next(const std::string& outer_block_name)
 {
@@ -42,5 +43,5 @@ std::shared_ptr<Block> Reader::next(const std::string& outer_block_name)
         }
     }
 }
-}
-}
+} // namespace mae
+} // namespace schrodinger
