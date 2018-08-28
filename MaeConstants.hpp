6f103dbd212678e6dc475940b71a0b72671df5b6
Pat Lorton <pat.lorton@schrodinger.com>
2018-08-28 10:58:34 -0400

    Switch to using constexpr const char *

--- MaeConstants.hpp
+++ MaeConstants.hpp
@@ -6,20 +6,20 @@ namespace schrodinger
 namespace mae
 {
 
-const std::string MAE_FORMAT_VERSION = "s_m_m2io_version";
-const std::string CT_BLOCK = "f_m_ct";
-const std::string CT_TITLE = "s_m_title";
-const std::string ATOM_BLOCK = "m_atom";
-const std::string ATOM_ATOMIC_NUM = "i_m_atomic_number";
-const std::string ATOM_X_COORD = "r_m_x_coord";
-const std::string ATOM_Y_COORD = "r_m_y_coord";
-const std::string ATOM_Z_COORD = "r_m_z_coord";
-const std::string ATOM_FORMAL_CHARGE = "i_m_formal_charge";
-const std::string ATOM_PARTIAL_CHARGE = "r_m_charge1";
-const std::string BOND_BLOCK = "m_bond";
-const std::string BOND_ATOM_1 = "i_m_from";
-const std::string BOND_ATOM_2 = "i_m_to";
-const std::string BOND_ORDER = "i_m_order";
+constexpr const char* MAE_FORMAT_VERSION = "s_m_m2io_version";
+constexpr const char* CT_BLOCK = "f_m_ct";
+constexpr const char* CT_TITLE = "s_m_title";
+constexpr const char* ATOM_BLOCK = "m_atom";
+constexpr const char* ATOM_ATOMIC_NUM = "i_m_atomic_number";
+constexpr const char* ATOM_X_COORD = "r_m_x_coord";
+constexpr const char* ATOM_Y_COORD = "r_m_y_coord";
+constexpr const char* ATOM_Z_COORD = "r_m_z_coord";
+constexpr const char* ATOM_FORMAL_CHARGE = "i_m_formal_charge";
+constexpr const char* ATOM_PARTIAL_CHARGE = "r_m_charge1";
+constexpr const char* BOND_BLOCK = "m_bond";
+constexpr const char* BOND_ATOM_1 = "i_m_from";
+constexpr const char* BOND_ATOM_2 = "i_m_to";
+constexpr const char* BOND_ORDER = "i_m_order";
 
 } // End namespace mae
 } // End namespace schrodinger
