f4a956c52aa077a1a408427bab12fc36e74b7598
Ric <ricrogz@gmail.com>
2019-09-03 19:49:41 -0400

    fix typos, mark s_st_AtomNumChirality_ deprecated

--- MaeConstants.hpp
+++ MaeConstants.hpp
@@ -28,15 +28,16 @@ const char* const BOND_ORDER = "i_m_order";
  * prevent the property names from clashing), e.g.:
  *
  * s_st_Chirality_1 :        2_R_1_3_5_4
- * s_st_AtomNumChirality_4:  6_ANR_1_5_7
+ * s_st_Chirality_2 :        6_ANR_1_5_7
  *
  * Syntax of chirality and pseudochirality properties is the same:
  *  1. Index of the chiral/pseudochiral atom.
  *  2. Chiral/pseudochiral label (R/S or ANR/ANS).
  *  3. Sequence of atoms around the chiral one, ordered by decreasing
- *     Cahn-Ingold-Prelog rank.
- *  Elements are separeted by underscores. For cases in which only 3 neighboring
+ *     Cahn-Ingold-Prelog rank, or atom index, in the case of ANR/ANS.
+ *  Elements are separated by underscores. For cases in which only 3 neighboring
  *  indexes are specified, an implicit Hydrogen atom needs to be considered.
+ *
  *
  * s_st_EZ_2 :               1_2_3_4_E
  *
@@ -48,11 +49,19 @@ const char* const BOND_ORDER = "i_m_order";
  *     allene-like structures)
  *  3. Highest C-I-P ranked stereogenical atom at the other side of the group.
  *  4. Stereochemical label for the bond (E/Z)
- *  As in chiral/pseudochiral properties, values are separeted by underscores.
+ *  As in chiral/pseudochiral properties, values are separated by underscores.
  */
 const std::string CT_CHIRALITY_PROP_PREFIX = "s_st_Chirality_";
-const std::string CT_PSEUDOCHIRALITY_PROP_PREFIX = "s_st_AtomNumChirality_";
 const std::string CT_EZ_PROP_PREFIX = "s_st_EZ_";
+
+/**
+ * This prefix has been deprecated, and will not be used in future Schrodinger
+ * Suite releases. It is included in this header only for the purpose of
+ * documentation and backwards compatibility. The format of the property string
+ * is identical as the one presented in the example for chirality.
+ */
+const std::string CT_PSEUDOCHIRALITY_PROP_PREFIX = "s_st_AtomNumChirality_";
+
 
 } // End namespace mae
 } // End namespace schrodinger
