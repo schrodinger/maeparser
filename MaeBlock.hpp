8d546978f2c84040723dcbd4c0aa80daca1e3973
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2021-10-24 14:18:25 +0900

    clang-tidy: readability-make-member-function-const

--- MaeBlock.hpp
+++ MaeBlock.hpp
@@ -43,7 +43,7 @@ class EXPORT_MAEPARSER IndexedBlockMapI
     getIndexedBlock(const std::string& name) const = 0;
 
     virtual std::vector<std::string> getBlockNames() const = 0;
-    bool operator==(const IndexedBlockMapI& rhs);
+    bool operator==(const IndexedBlockMapI& rhs) const;
 };
 
 class EXPORT_MAEPARSER IndexedBlockMap : public IndexedBlockMapI
